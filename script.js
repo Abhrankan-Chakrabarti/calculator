f9a88d6887b07548a15da7985f627b3411c339f1
Abhrankan Chakrabarti <abhrankan@gmail.com>
2026-05-20 11:49:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-const display = document.querySelector('#display')
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
 
-const buttons = document.querySelectorAll('button')
-
-
-buttons.forEach((item)=>{
-    item.onclick = ()=>{
-        if(item.id=='clear'){
-            display.innerText = ''
+buttons.forEach((item) => {
+    item.onclick = () => {
+        // Prevent appending numbers while the warning message is visible
+        if (display.innerText === 'Please Enter Something to Calculate' || display.innerText === 'Error') {
+            display.innerText = '';
         }
 
-        else if(item.id=='backspace'){
-             let string  = display.innerText.toString()
-             display.innerText = string.substr(0 , string.length-1)
+        if (item.id === 'clear') {
+            display.innerText = '';
+        } 
+        else if (item.id === 'backspace') {
+            let string = display.innerText.toString();
+            display.innerText = string.substring(0, string.length - 1);
+        } 
+        else if (display.innerText !== '' && item.id === 'equal') {
+            try {
+                // Safeguard against bad expressions like "5++" or "8*"
+                display.innerText = eval(display.innerText);
+            } catch (error) {
+                display.innerText = 'Error';
+                setTimeout(() => (display.innerText = ''), 1500);
+            }
+        } 
+        else if (display.innerText === '' && item.id === 'equal') {
+            display.innerText = 'Please Enter Something to Calculate';
+            setTimeout(() => (display.innerText = ''), 2000);
+        } 
+        else {
+            display.innerText += item.id;
         }
-
-        else if(display.innerText!='' && item.id == 'equal'){
-               display.innerText = eval(display.innerText)
-        }
-
-        else if(display.innerText=='' && item.id=='equal'){
-            display.innerText = 'Please Enter Something to Calculate'
-            setTimeout(()=>(display.innerText=''), 2000)
-
-        }
-        
-        else{
-            display.innerText+= item.id
-        }
-    }
-})
+    };
+});
